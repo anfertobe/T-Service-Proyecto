30c316585241264985d70ad79719a3bb5ed62e74
luchinho9321 <lagcoronell@gmail.com>
2015-03-19 12:25:10 -0500

    Luis Andrés Gómez
    licencia vigente en función por aparte.

--- src/main/java/com/tservice/Logica/PersistenceFacede.java
+++ src/main/java/com/tservice/Logica/PersistenceFacede.java
@@ -105,25 +105,8 @@ public class PersistenceFacede {
         if(this.publicru.exists(pu.getIdentificacion())){
             Publicante puBD=this.publicru.findOne(pu.getIdentificacion());
             
-            //Traer factura actual
-            Factura facturaActual=getFacturaActual((ArrayList<Factura>) puBD.getFacturas());
-            
-            //Si tiene factura actual
-            if(facturaActual!=null){
-                
-                Date fechaActual=facturaActual.getFecha();
-                int vigenciaDias=facturaActual.getLicencias().getVigenciaDias();
-                Date fechaVencimiento;
-                
-                
-                //Sumar dias a fecha
-                Calendar calendar = Calendar.getInstance();
-                calendar.setTime(fechaActual);
-                calendar.add(Calendar.DAY_OF_YEAR, vigenciaDias);
-                fechaVencimiento=calendar.getTime();
-
-                //Si la fecha de vencimiento es menor o igual a la fecha actual se agrega la oferta
-                if(fechaVencimiento.before(new Date()) || fechaVencimiento.equals(new Date()) ){
+                //Validar vigencia
+                if(licenciaVigente(puBD)){
                
                     //Salvar oferta
                     of.setPublicante(puBD);
@@ -143,12 +126,6 @@ public class PersistenceFacede {
                     //La licencia actual del publicante ya no tiene vigencia
                     comentario="La licencia actual del publicante ya no tiene vigencia";
                 }
-            
-            }else{
-                //El publicante no tiene facturas ni licencias
-                comentario="El publicante no tiene facturas ni licencias";
-            }
-            
         }else{
             //El publicante no existe en el sistema
             comentario="El publicante no existe en el sistema";
@@ -156,6 +133,37 @@ public class PersistenceFacede {
                
         return comentario;
     }
+    
+    
+    private boolean licenciaVigente(Publicante pu){
+        
+            boolean vigente=false;
+            //Traer factura actual
+            Factura facturaActual=getFacturaActual((ArrayList<Factura>) pu.getFacturas());
+            
+            //Si tiene factura actual
+            if(facturaActual!=null){
+                
+                Date fechaActual=facturaActual.getFecha();
+                int vigenciaDias=facturaActual.getLicencias().getVigenciaDias();
+                Date fechaVencimiento;
+                
+                
+                //Sumar dias a fecha
+                Calendar calendar = Calendar.getInstance();
+                calendar.setTime(fechaActual);
+                calendar.add(Calendar.DAY_OF_YEAR, vigenciaDias);
+                fechaVencimiento=calendar.getTime();
+
+                //Si la fecha de vencimiento es menor o igual a la fecha actual
+                vigente=fechaVencimiento.before(new Date()) || fechaVencimiento.equals(new Date());
+                        
+               
+            }
+        return vigente;
+    }
+    
+    
     
     private Factura getFacturaActual(ArrayList<Factura> facturas){
         
@@ -255,11 +263,54 @@ public class PersistenceFacede {
     *@obj: aplicar a oferta postulante
     *@param: postulante , oferta
     *@pre: El postulante ya existe , la oferta ya existe
-    *@return: si aplico oferta o no
+    *@return: Comentario
     */
-    public boolean aplicarOferta(Postulante po,Oferta of)
+    public String aplicarOferta(Postulante po,Oferta of)
     {
-        return false;
+        String comentario="OK";
+        
+        //Si el postulante y ademas la oferta existe
+        if(this.postCru.exists(po.getIdentificacion()) &&  this.oferCru.exists(of.getId()) ){
+            //Trae oferta de BD
+            Oferta ofBD=this.oferCru.findOne(of.getId());
+            
+            //Si el publicante existe
+            if(this.publicru.exists(ofBD.getPublicante().getIdentificacion())){
+                //Traer publicante de BD
+                Publicante puBD=this.publicru.findOne(ofBD.getPublicante().getIdentificacion());
+                
+                if(licenciaVigente(puBD)){
+                    
+                    //Trae postulande de BD
+                    Postulante poBD=this.postCru.findOne(po.getIdentificacion());
+                    
+                    //Si tiene una licencia vigente se asocia a la oferta
+                    Set<Postulante> postulantes=ofBD.getPostulantes();
+                    
+                    //Agregar a postulantes
+                    postulantes.add(poBD);
+                    
+                    
+                    //Asociar postulantes
+                    ofBD.setPostulantes(postulantes);
+                    
+                    //Salvar oferta
+                    this.oferCru.save(ofBD);
+                    
+                }else{
+                    //El publicante de la oferta no existe
+                    comentario="La oferta expiró";
+                }
+            }else{
+                //El publicante de la oferta no existe
+                comentario="El publicante de la oferta no existe";
+            }
+        }else{
+            //El publicante de la oferta no existe
+            comentario="El postulante o la oferta no existen";
+        }
+        
+        return comentario;
     }  
 
     /*
