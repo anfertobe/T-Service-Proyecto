816772a852a9d7fc16c6e3aa85dfc49f71d43d5b
anfertobe <anfertobe@gmail.com>
2015-05-18 20:02:54 -0500

    AFT
    arreglo presentacion

--- src/main/webapp/js/libs/appmodule.js
+++ src/main/webapp/js/libs/appmodule.js
@@ -21,8 +21,8 @@
 
                     })
 
-                    .when('/login', {
-                        templateUrl: 'login.html'
+                    .when('/home', {
+                        templateUrl: 'home.html'
 
                     })
 
@@ -46,7 +46,7 @@
                         templateUrl: 'MNLogin.html'
                     })
                     .otherwise({
-                        redirectTo: '/MNLogin'
+                        redirectTo: '/home'
                     });
         }]);
 
